8b626fa2d26ea177be244406e38c997759cc7f77
JuanDavid <juandavidgodoy@hotmail.com>
2021-01-28 16:30:51 -0500

    decimales

--- ConversorDeMonedas.py
+++ ConversorDeMonedas.py
@@ -2,5 +2,6 @@ pesos = input("Cantidad de pesos colombianos: ")
 pesos = float(pesos)
 valor_dolar = 3560 
 dolares = pesos / valor_dolar
+dolares = round(dolares, 3)
 dolares = str(dolares)
 print("Tienes una cantiad de $" + dolares + " dólares")
