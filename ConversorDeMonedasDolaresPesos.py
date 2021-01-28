5c715a46734ce1e172733e87f4fe093efa774642
JuanDavid <juandavidgodoy@hotmail.com>
2021-01-28 16:53:05 -0500

    dolares a pesos

---
+++ ConversorDeMonedasDolaresPesos.py
@@ -0,0 +1,7 @@
+dolares = input("Cantidad de dolares: ")
+dolares = float(dolares)
+valor_pesos = 3560 
+pesos = dolares * valor_pesos
+pesos = round(pesos, 3)
+pesos = str(pesos)
+print("Tienes una cantiad de $" + pesos + " pesos")
