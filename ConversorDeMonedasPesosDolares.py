2f66d11db29886e15add97f37d476bcdce0f40e3
JuanDavid <juandavidgodoy@hotmail.com>
2021-01-29 17:39:00 -0500

    cambios hehcoa

--- ConversorDeMonedasPesosDolares.py
+++ ConversorDeMonedasPesosDolares.py
@@ -1,14 +1,29 @@
+def conversor(tipo_pesos, valor_dolar):
+    pesos = input("Cantidad de pesos colombiano" + tipos_pesos + ": ")
+    pesos = float(pesos) 
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 3)
+    dolares = str(dolares)
+    print("Tienes una cantiad de $" + dolares + " dolares")
+
 menu = input("""Bienevenido al conversor de texto
 (1)Colombia
 (2)Argentina
 (3)Mexico
 Elija la opcion de su pais: """)
-pesos = input("Cantidad de pesos colombianos: ")
-pesos = float(pesos)
-valor_dolar = 3560 
-dolares = pesos / valor_dolar
-dolares = round(dolares, 3)
-dolares = str(dolares)
-print("Tienes una cantiad de $" + dolares + " dolares")
+
+if menu == "1":
+    conversor("colombianos", 3560 )
+elif menu == "2":
+    conversor("argentinos", 87)
+elif menu == "3":
+    conversor("mexicanos", 20)
+else:
+    print("Ingrese una opcion correcta")
+
+
+
+
+
 
 
