11ed05229f714c1cf573a58b92a7051bafc8f1bc
JuanDavid <juandavidgodoy@hotmail.com>
2021-01-29 17:21:45 -0500

    a2

--- ConversorDeMonedasPesosDolares.py
+++ ConversorDeMonedasPesosDolares.py
@@ -3,14 +3,12 @@ menu = input("""Bienevenido al conversor de texto
 (2)
 (3)
 Elija la opcion de su pais: """)
-
-
-    pesos = input("Cantidad de pesos colombianos: ")
-    pesos = float(pesos)
-    valor_dolar = 3560 
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 3)
-    dolares = str(dolares)
-    print("Tienes una cantiad de $" + dolares + " dolares")
+pesos = input("Cantidad de pesos colombianos: ")
+pesos = float(pesos)
+valor_dolar = 3560 
+dolares = pesos / valor_dolar
+dolares = round(dolares, 3)
+dolares = str(dolares)
+print("Tienes una cantiad de $" + dolares + " dolares")
 
 
